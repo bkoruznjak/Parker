ffdc1d8be979ee47a832e051f774b6dc208040d0
bkoruznjak <borna.koruznjak@gmail.com>
2018-07-06 12:14:38 +0200

    - cleanup and moved to kotlin

---
+++ app/src/main/java/bkoruznjak/from/hr/parker/view/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package bkoruznjak.from.hr.parker.view.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import bkoruznjak.from.hr.parker.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
